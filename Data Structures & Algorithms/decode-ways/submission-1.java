6bd370120353c402b8d6f967b820ecf1d7abf6d1
Abhishek Kapoor <83063996+abhishekkapoorx@users.noreply.github.com>
2026-06-21 21:35:56 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int decode(String s, int i, int[] dp) {
+        if (i == s.length()) return 1;
+        if (s.charAt(i) == '0') return 0;
+
+        if (dp[i] != -1) return dp[i];
+        int takeOne = s.charAt(i) != 0 ? decode(s, i+1, dp) : 0;
+        int takeTwo = 0;
+        if (
+            s.charAt(i) != '0' && 
+            s.length() - i >= 2 && 
+            Integer.parseInt(s.substring(i, i+1)) <= 26
+        ) {
+            takeTwo = decode(s, i+2, dp);
+        }
+        return dp[i] = takeOne + takeTwo;
+    }
+    public int numDecodings(String s) {
+        int dp[] = new int[s.length()];
+        Arrays.fill(dp, -1);
+
+        int ways = decode(s, 0, dp);
+        return ways;
+    }
+}
